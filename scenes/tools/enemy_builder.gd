b92cedabc4b53f88ee7e2d19f71e3060fbce602b
AA <aa@aa.com>
2025-06-08 15:10:12 +0200

    first version of enemy builder

---
+++ scenes/tools/enemy_builder.gd
@@ -0,0 +1,18 @@
+extends Control
+
+var enemy_bot_definition = null
+
+func _ready() -> void:
+	enemy_bot_definition = BotDefinition.new()
+	enemy_bot_definition.hull = Hull.new()
+
+
+func _on_save() -> void:
+	if ($Name.text == ""): 
+		return
+		
+	enemy_bot_definition.hull.name = $Name.text
+	enemy_bot_definition.hull.health = $Hull/Health.value
+	enemy_bot_definition.hull.speed = $Hull/Speed.value
+
+	ResourceSaver.save(enemy_bot_definition, "test_enemy.tres",  )
