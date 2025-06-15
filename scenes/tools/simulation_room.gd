a498af451220f26aed95fb4029a75f3bac95e197
Hakuraph <raphael.adensamer@emini.at>
2025-06-15 18:25:59 +0200

    bugfix at simulation rom

--- scenes/tools/simulation_room.gd
+++ scenes/tools/simulation_room.gd
@@ -1,34 +1,38 @@
 extends Node2D
 
-const Weapons = {
-	"res://scenes/bots/parts/weapon_t1_big_cannon.tres" : "big_cannon 1",
-	"res://scenes/bots/parts/weapon_t2_big_cannon.tres" : "big_cannon 2",
-	"res://scenes/bots/parts/weapon_t3_big_cannon.tres" : "big_cannon 3",
-	"res://scenes/bots/parts/weapon_t1_bomb.tres" : "bomb 1",
-	"res://scenes/bots/parts/weapon_t2_bomb.tres" : "bomb 2",
-	"res://scenes/bots/parts/weapon_t3_bomb.tres" : "bomb 3",
-	"res://scenes/bots/parts/weapon_t1_cannon.tres" : "cannon 1",
-	"res://scenes/bots/parts/weapon_t2_cannon_fast.tres" : "cannon 2",
-	"res://scenes/bots/parts/weapon_t1_laser.tres" : "laser 1",
-	"res://scenes/bots/parts/weapon_t2_laser.tres" : "laser 2",
-	"res://scenes/bots/parts/weapon_t3_laser.tres" : "laser 3",
-	"res://scenes/bots/parts/weapon_t1_pea_shooter.tres" : "pea shooter 1",
+const Weapons_wins = {
+	"res://scenes/bots/parts/weapon_t1_big_cannon.tres" : 0,
+	"res://scenes/bots/parts/weapon_t2_big_cannon.tres" : 0,
+	"res://scenes/bots/parts/weapon_t3_big_cannon.tres" : 0,
+	"res://scenes/bots/parts/weapon_t1_bomb.tres" : 0,
+	"res://scenes/bots/parts/weapon_t2_bomb.tres" : 0,
+	"res://scenes/bots/parts/weapon_t3_bomb.tres" : 0,
+	"res://scenes/bots/parts/weapon_t1_cannon.tres" : 0,
+	"res://scenes/bots/parts/weapon_t2_cannon_fast.tres" : 0,
+	"res://scenes/bots/parts/weapon_t1_laser.tres" : 0,
+	"res://scenes/bots/parts/weapon_t2_laser.tres" : 0,
+	"res://scenes/bots/parts/weapon_t3_laser.tres" : 0,
+	"res://scenes/bots/parts/weapon_t1_pea_shooter.tres" : 0,
 }
+var weapon_list = Weapons_wins.keys()
 var used_weapons = 0
 
 const CUSTOM_BOT_SCENE = preload("res://scenes/bots/custom_bot.tscn")
 @export var hull : Hull
 
+func _ready() -> void:
+	spawn_bot(get_new_player_bot_defenition(), $playing_field/Startposition.global_position)
+
 
 func get_new_player_bot_defenition():
 	var bot_definition = BotDefinition.new()
 	bot_definition.hull = hull
-	bot_definition.weapons.clear()
-	bot_definition.weapons.append(Weapons[used_weapons])
-	return BotDefinition
+	bot_definition.weapons.clear()	
+	bot_definition.weapons.append(weapon_list[used_weapons])
+	return bot_definition
 
-func spawn_bot(bot_definition, position:Vector2):
-	var bot_def = load(bot_definition)
+func spawn_bot(bot_definition, position:Vector2): 
+	var bot_def = bot_definition
 		
 	var bot = null
 	if (bot_def is BotDefinition):
