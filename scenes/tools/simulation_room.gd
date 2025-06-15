3d05b6431c5ef2759a7c57db5db1475b225eacb3
Hakuraph <raphael.adensamer@emini.at>
2025-06-15 19:36:16 +0200

    finished first version of simulation room !not testet!

--- scenes/tools/simulation_room.gd
+++ scenes/tools/simulation_room.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-const Weapons_wins = {
+var Weapons_wins = {
 	"res://scenes/bots/parts/weapon_t1_big_cannon.tres" : 0,
 	"res://scenes/bots/parts/weapon_t2_big_cannon.tres" : 0,
 	"res://scenes/bots/parts/weapon_t3_big_cannon.tres" : 0,
@@ -16,27 +16,65 @@ const Weapons_wins = {
 }
 var weapon_list = Weapons_wins.keys()
 var used_weapons = 0
+var curr_rounds_done = 0
 
 const CUSTOM_BOT_SCENE = preload("res://scenes/bots/custom_bot.tscn")
 @export var hull : Hull
+@export var enemy_bot_defenition : BotDefinition
+
+var bot1
+var bot2
+
 
 func _ready() -> void:
-	spawn_bot(get_new_player_bot_defenition(), $playing_field/Startposition.global_position)
+	start_new_round()
+
+
+func start_new_round():
+	spawn_bot(get_new_player_bot_defenition(), $playing_field/Startposition.global_position, on_bot_1_died, 1)
+	spawn_bot(enemy_bot_defenition, $playing_field/Startposition2.global_position, on_bot_2_died, 2)
+	curr_rounds_done += 1
+	if curr_rounds_done == 100:
+		curr_rounds_done = 0
+		used_weapons += 1
+		if used_weapons >= weapon_list.size():
+			print(Weapons_wins)
+			clear_bots()
 
 
 func get_new_player_bot_defenition():
 	var bot_definition = BotDefinition.new()
 	bot_definition.hull = hull
 	bot_definition.weapons.clear()	
-	bot_definition.weapons.append(weapon_list[used_weapons])
+	var weapon = weapon_list[used_weapons]
+	bot_definition.weapons.append(load(weapon))
 	return bot_definition
 
-func spawn_bot(bot_definition, position:Vector2): 
-	var bot_def = bot_definition
-		
-	var bot = null
+func spawn_bot(bot_def, position:Vector2, signal_, bot_number): 
 	if (bot_def is BotDefinition):
-		bot = CUSTOM_BOT_SCENE.instantiate()
+		var bot = CUSTOM_BOT_SCENE.instantiate()
 		bot.set_bot_definition(bot_def)
 		bot.global_position = position 
-		add_child(bot)
+		bot.connect("died", signal_)
+		$bot_root.add_child(bot)
+		if bot_number == 1:
+			bot1 = bot
+		else:
+			bot2 = bot
+
+func clear_bots():
+	if bot1:
+		bot1.queue_free()
+	if bot2:
+		bot2.queue_free()
+
+
+
+func on_bot_1_died():
+	Weapons_wins[weapon_list[used_weapons]] += 1
+	clear_bots()
+	start_new_round()
+
+func on_bot_2_died():
+	clear_bots()
+	start_new_round()
